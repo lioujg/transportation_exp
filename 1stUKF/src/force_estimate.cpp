8c0166019f38fb1bc7fe806404ee79f0e81ef40d
lioujg <lioujg870117@gmail.com>
2021-07-20 15:53:27 +0800

    cancel the fixed thrust

--- 1stUKF/src/force_estimate.cpp
+++ 1stUKF/src/force_estimate.cpp
@@ -64,17 +64,17 @@ void imu_cb(const sensor_msgs::Imu::ConstPtr &msg){
 
 Eigen::Vector3d thrust, last_thrust;
 void thrust_cb(const geometry_msgs::WrenchStamped::ConstPtr &msg){
-  // if(isnan(msg->wrench.force.z) == 0){
-  //   thrust << msg->wrench.force.x, msg->wrench.force.y, msg->wrench.force.z;}
-  // else if(isnan(msg->wrench.force.z) != 0){
-  //   thrust(2) = last_thrust(2);
-  //   std::cout << "I meet something cool like nan!!" << std::endl;
-  //   nan_count++;
-  // }
-  // std::cout << thrust(2) << std::endl;
-  // last_thrust = thrust;
+  if(isnan(msg->wrench.force.z) == 0){
+    thrust << msg->wrench.force.x, msg->wrench.force.y, msg->wrench.force.z;}
+  else if(isnan(msg->wrench.force.z) != 0){
+    thrust(2) = last_thrust(2);
+    std::cout << "I meet something cool like nan!!" << std::endl;
+    nan_count++;
+  }
+  std::cout << thrust(2) << std::endl;
+  last_thrust = thrust;
 
-  thrust << 0, 0, 23;
+  //thrust << 0, 0, 23;
 }
 
 float ground_truth_yaw;
